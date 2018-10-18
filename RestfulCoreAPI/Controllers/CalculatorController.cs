196d663f98639c82ab61e3f1d655ac4d39928afa
jimmyrl19 <jimmy.rl19@hotmail.com>
2018-10-17 23:11:34 -0300

    first api

---
+++ RestfulCoreAPI/Controllers/CalculatorController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestfulCoreAPI.Controllers
+{
+    [Route("api/operations")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid input.");
+        }
+
+        [HttpGet("sub/{firstNumber}/{secondNumber}")]
+        public IActionResult Subtract(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sub = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(sub.ToString());
+            }
+
+            return BadRequest("Invalid input.");
+        }
+
+        [HttpGet("mult/{firstNumber}/{secondNumber}")]
+        public IActionResult Multiplication(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var mult = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(mult.ToString());
+            }
+
+            return BadRequest("Invalid input.");
+        }
+
+        [HttpGet("div/{firstNumber}/{secondNumber}")]
+        public IActionResult Div(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var div = 0M;
+                if(ConvertToDecimal(secondNumber) != 0)
+                {
+                    div = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+                }
+                
+                return Ok(div.ToString());
+            }
+
+            return BadRequest("Invalid input.");
+        }
+
+        private decimal ConvertToDecimal(string firstNumber)
+        {
+            decimal decimalValue;
+
+            if(decimal.TryParse(firstNumber, out decimalValue))
+            {
+                return decimalValue;
+            }
+
+            return 0;
+        }
+
+        private bool IsNumeric(string number)
+        {
+            decimal decimalNumber;
+
+            bool isNumber = decimal.TryParse(
+                number, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out decimalNumber);
+
+            return isNumber;
+        }
+    }
+}
